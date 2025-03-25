9f5e3332aee45493973b48c57caf5bc095f0c131
gicorada <me@gicorada.com>
2025-03-25 08:49:43 +0100

    feat: added local db connection for development environment

--- utils/conn.php
+++ utils/conn.php
@@ -1,8 +1,18 @@
 <?php
-    $hostname = "localhost";
-    $username = "phpmyadmin";
-    $password  = "dietpi";
-    $dbname = "Polisportiva";
+    // Controlla se l'ambiente è di sviluppo o di produzione tramite l'uso del
+    // file "isDev", che non viene sincronizzato con git e quindi non sarà 
+    // presente nel server
+    if(!file_exists("../isDev")) {
+        $hostname = "localhost";
+        $username = "phpmyadmin";
+        $password  = "dietpi";
+        $dbname = "Polisportiva";
+    } else {
+        $hostname = "localhost";
+        $username = "root";
+        $password  = "";
+        $dbname = "Polisportiva";
+    }
 
     $conn = new mysqli($hostname, $username, $password, $dbname) or die("connection error!");
 

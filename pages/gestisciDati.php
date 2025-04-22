3686e0e0a29d745cbb5cebb95fe4161c95238258
gicorada <me@gicorada.com>
2025-04-22 10:37:07 +0200

    Aggiunta pagina per fetch dati utente

--- pages/gestisciDati.php
+++ pages/gestisciDati.php
@@ -14,8 +14,77 @@ $stmt->execute();
 
 $result = $stmt->get_result();
 $row = $result->fetch_assoc();
-
-foreach($result as $row){
-    var_dump($row);
-}
 ?>
+
+<?php foreach($result as $row):?>
+	
+    <table border=1px>
+
+		<thead>
+
+			<tr>
+
+				<th>
+					Nome
+				</th>
+
+				<th>
+					Cognome
+				</th>
+
+				<th>
+					Email
+				</th>
+
+				<th>
+					TipoAtleta
+				</th>
+
+				<th>
+					Personale
+				</th>
+
+				<th>
+					SportPraticato
+				</th>
+
+			</tr>
+
+		</thead>
+
+
+
+
+		 <tbody>
+
+			 <tr>
+				<td>
+					<?= $row['Nome'] ?>
+				</td>
+
+			 	<td>
+				 	<?= $row['Cognome'] ?>
+				</td>
+
+			 	<td>
+				 	<?= $row['Email'] ?>
+				</td>
+
+			 	<td>
+				 	<?= $row['TipoAtleta'] ?>
+				</td>
+
+			 	<td>
+				 	<?= $row['TipoPersonale'] ?>
+				</td>
+
+			 	<td>
+				 	<?= $row['SportPraticato'] ?>
+				</td>
+			 </tr>
+
+		 </tbody>
+
+	 </table>
+
+<?php endforeach; ?>

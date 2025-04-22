6a0b8b872a3831f7bc3061df021897fe8c10c75e
gicorada <me@gicorada.com>
2025-04-22 10:37:20 +0200

    Migliorata gestione assemblee con calendario

--- pages/visualizzaAssemblee.php
+++ pages/visualizzaAssemblee.php
@@ -8,30 +8,64 @@ CALENDARIO CON ASSEMBLEE
 <?php
 include "../utils/conn.php";
 include "../utils/verifyAndStartSession.php";
-/*require_once "../utils/requireCalendar.php";
 
-$calendar = new Calendar;
-$calendar->stylesheet();
-*/
-
-$query = "SELECT Data, ODG, Descrizione, CONCAT(P.Nome, ' ', P.Cognome) as Convocatore
+$query = "SELECT Codice, Data, ODG, Descrizione, CONCAT(P.Nome, ' ', P.Cognome) as Convocatore, P_A.Persona as Partecipa
 			FROM ASSEMBLEA A
 			JOIN PERSONA P ON (A.Convocatore = P.CF)
-			/*LEFT JOIN PARTECIPAZIONE_ASSEMBLEA P_A ON (P.CF = P_A.Persona)*/;";
+			LEFT JOIN PARTECIPAZIONE_ASSEMBLEA P_A ON (A.Codice = P_A.Assemblea AND ? = P_A.Persona);";
 
 $stmt = $conn->prepare($query);
-/*$stmt->bind_param("s", $_SESSION["CF"]);*/
+$stmt->bind_param("s", $_SESSION["CF"]);
 $stmt->execute();
 
 $result = $stmt->get_result();
-$row = $result->fetch_assoc();
-
-$result = $conn->query($query);
-
-foreach($result as $row){
-    var_dump($row);
-}
-
-/*(new Calendar)->display();*/
+/*
+foreach($result as $row) {
+	var_dump($row);
+}*/
 
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@event-calendar/build@3.12.0/event-calendar.min.css">
+		<script src="https://cdn.jsdelivr.net/npm/@event-calendar/build@3.12.0/event-calendar.min.js"></script>
+		<link rel="stylesheet" href="../style/base.css">
+	</head>
+	<body>
+		<main>
+			<div id="ec"></div>
+		</main>
+
+		<script>
+			let ec = new EventCalendar(document.getElementById('ec'), {
+				view: 'timeGridWeek',
+				events: [
+					<?php foreach($result as $row): ?>
+						{
+							id: <?= $row["Codice"] ?>,
+							allDay: false,
+							start: new Date("<?= $row["Data"] ?>"),
+							end: addDefaultEventDuration(new Date("<?= $row["Data"] ?>")),
+							title: "<?= $row["Descrizione"] ?>",
+							display: "auto",
+							editable: false,
+							startEditable: false,
+							durationEditable: false,
+							backgroundColor: (("<?= $row["Partecipa"] ?>" != "") ? "green" : "red")
+						},
+					<?php endforeach; ?>
+				],
+				height: "80vh",
+				nowIndicator: true,
+			});
+
+			function addDefaultEventDuration(date) {
+				let endDate = date;
+				endDate.setHours(endDate.getHours() + 1);
+				return endDate;
+			}
+		</script>
+	</body>
+</html>
